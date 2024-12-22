7db76d7cf47d46f338f775127fa386900679ca31
Muhammad Zawar <mzawar189@gmail.com>
2024-12-22 22:23:15 +0500

    check issue

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: ".", // Set the root directory
+  build: {
+    outDir: "dist", // Specify the output directory
+  },
+});
