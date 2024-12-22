ee06061432457882ee9e59ae0d4f83e1a3f241e2
Muhammad Zawar <mzawar189@gmail.com>
2024-12-23 00:11:52 +0500

    add arabic

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "./index.html", // Set the root directory
+  root: "public", // Set "public" as the root directory for development
   build: {
-    outDir: "dist", // Specify the output directory
+    outDir: "../dist", // Output build files in a sibling "dist" folder
+    emptyOutDir: true, // Clear the "dist" folder before each build
+  },
+  server: {
+    open: true, // Automatically open the browser on localhost
   },
 });
